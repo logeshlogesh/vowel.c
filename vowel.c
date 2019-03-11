8fb1a5d03ca80fad2b1b8b1ea0e9224e3b4c7474
logeshlogesh <48052938+logeshlogesh@users.noreply.github.com>
2019-03-11 16:36:26 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+char x;
+printf("enter the number");
+scanf("%c",&x);
+if(x='a' || x='e' || x='i' || x='o' || x='u')
+{
+printf("vowels");
+}
+else
+{
+printf("consonant");
+}
+}
